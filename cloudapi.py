7ed99ee7c3623693d91a573d10397aa01fe4ddf8
Zhaoxiong Ding <zhaoxiong.ding@yale.edu>
2017-12-03 02:38:15 -0500

    Added cloudapi.py which opens policy files and outputs them into raw files. A policy file is just a plain text file for each website. A raw file is the raw response from Google's natrual language API entity analysis.

---
+++ cloudapi.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import pdb
+
+# Opening file and reading policy
+currWebsite = 'twitter'
+f = open('Corpus/'+currWebsite+'.policy','r')
+text=f.readlines()
+text='\n'.join(text)
+f.close()
+
+# Google Cloud API
+api_url = 'https://language.googleapis.com/v1/documents:analyzeEntities?key=AIzaSyDZUZaUB5Oz52iTRk4KYYNjqGtV6XLXEV8'
+
+data = {'document':{'type':'PLAIN_TEXT','content':text},'encodingType':'UTF8'}
+
+response = requests.post(api_url, json.dumps(data))
+
+# Writing response
+f = open('Corpus/'+currWebsite+'.raw','w+')
+f.write(response.text)
+f.close()
